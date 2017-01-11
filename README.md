19fc1957b37dad5289cdc7d4e0327ac57b66c232
jheddings <jason@heddings.com>
2017-01-11 15:52:47 -0700

    First loop through the data set

---
+++ README.md
@@ -0,0 +1,21 @@
+# US Topo Downloader
+
+Catalog information: https://geonames.usgs.gov/pls/topomaps/
+
+[ScienceBase API](https://my.usgs.gov/confluence/display/sciencebase/ScienceBase)
+
+Use in accordance with the terms of the [USGS](https://www2.usgs.gov/faq/?q=categories/9797/3572).
+
+## Requirements
+
+[Parse::CSV](http://search.cpan.org/~kwilliams/Parse-CSV-2.04/lib/Parse/CSV.pm)
+
+## Usage
+
+TODO
+
+## Improvements
+
+* Use ScienceBase API directly, rather than CSV catalog
+* Generate browseable HTML file offline of maps
+
--- ustopo.pl
+++ ustopo.pl
@@ -1,14 +1,20 @@
 #!/usr/bin/perl -w
 
+# basic script for maintaining an offline copy of the US Topo map collection
+
 use strict;
 
 use Getopt::Std;
 use Parse::CSV;
+use File::Spec;
+use File::Path qw( mkpath );
+use File::Basename;
 
 # parse command line options TODO error checking
 
 # -C catalog file
 # -D data directory
+# TODO path format string for local file
 
 my %opts;
 
@@ -18,9 +24,28 @@ my $catalog = $opts{C};
 my $datadir = $opts{D};
 
 ################################################################################
+# generate the full file path for a given record - the argument is a hashref
+sub get_local_path {
+  my ($item) = @_;
+
+  # sanitize the map name to get a file name
+  my $filename = $item->{'Cell Name'};
+  $filename =~ s/[^A-Za-z0-9_ -]/_/g;
+  $filename .= '.pdf';
+
+  # should be safe, but sanitize anyway
+  my $state = $item->{'Primary State'};
+  $state =~ s/[^A-Za-z0-9._-]/_/g;
+
+  my $abs_datadir = File::Spec->rel2abs($datadir);
+
+  File::Spec->join($abs_datadir, $state, $filename);
+}
+
+################################################################################
 ## MAIN ENTRY
 
-my $items = Parse::CSV->new(
+my $csv = Parse::CSV->new(
   file => $catalog,
   names => 1,
 
@@ -31,6 +56,16 @@ my $items = Parse::CSV->new(
 );
 
 # run through the current items
-while (my $item = $items->fetch) {
-  printf("%s\n", $item->{'Map Name'});
+while (my $item = $csv->fetch) {
+  my $path = get_local_path($item);
+
+  # if the file exists TODO and is current
+  next if -f $path;
+
+  # make necessary directories
+  my $dirname = dirname($path);
+  mkpath($dirname);
+
+  printf("%s\n", $path);
 }
+
