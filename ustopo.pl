b1e2aba75c0f63f5701e21c12200b15a249f2f53
jheddings <jason@heddings.com>
2017-01-12 13:39:12 -0700

    Initial download skeleton

--- ustopo.pl
+++ ustopo.pl
@@ -9,19 +9,37 @@ use Parse::CSV;
 use File::Spec;
 use File::Path qw( mkpath );
 use File::Basename;
+use LWP::Simple;
+use LWP::UserAgent;
+
+use Log::Message::Simple qw[:STD :CARP];
+use Data::Dumper;
 
 # parse command line options TODO error checking
 
 # -C catalog file
 # -D data directory
+# -v debug logging
+# -q silent mode
+# -U custom user agent for download client
 # TODO path format string for local file
-
 my %opts;
 
-getopts('C:D:', \%opts);
+getopts('C:D:U:qv', \%opts);
 
-my $catalog = $opts{C};
-my $datadir = $opts{D};
+my $catalog = File::Spec->rel2abs($opts{C});
+my $datadir = File::Spec->rel2abs($opts{D});
+
+my $verbose = exists $opts{v};
+my $silent = exists $opts{q};
+my $debug = $verbose and not $silent;
+
+################################################################################
+# the common client for download files
+my $client = LWP::UserAgent->new;
+if (exists $opts{U}) {
+  $client->agent($opts{U});
+}
 
 ################################################################################
 # generate the full file path for a given record - the argument is a hashref
@@ -43,7 +61,42 @@ sub get_local_path {
 }
 
 ################################################################################
+# determines if the local copy of item is current
+sub current_item {
+  my ($item) = @_;
+  return undef;
+}
+
+################################################################################
+# download a specific item and return the path to the local file
+sub download_item {
+  my ($item) = @_;
+
+  my $path = get_local_path($item);
+
+  # download the file to a temp location
+  my $resp = $client->get($item->{'Download GeoPDF'});
+
+  if ($resp->is_success) {
+    print Dumper($resp->headers);
+  } else {
+    die $resp->status_line;
+  }
+
+  # make necessary directories
+  my $dirname = dirname($path);
+  mkpath($dirname);
+
+  # extract the file in the new folder
+
+  return $path;
+}
+
+################################################################################
 ## MAIN ENTRY
+
+msg('Using data directory: ' . $datadir, not $silent);
+msg('Loading catalog: ' . $catalog, not $silent);
 
 my $csv = Parse::CSV->new(
   file => $catalog,
@@ -57,15 +110,20 @@ my $csv = Parse::CSV->new(
 
 # run through the current items
 while (my $item = $csv->fetch) {
-  my $path = get_local_path($item);
+  my $mapname = sprintf('%s [%s]', $item->{'Map Name'}, $item->{'Cell ID'});
+  msg('Processing map: ' . $mapname, not $silent);
 
-  # if the file exists TODO and is current
-  next if -f $path;
+  my $local_file = current_item($item);
 
-  # make necessary directories
-  my $dirname = dirname($path);
-  mkpath($dirname);
+  if ($local_file) {
+    debug('=> Map is up to date: ' . $local_file, $local_file);
+  } else {
+    debug('=> Downloading map: ' . $item->{'Download GeoPDF'}, $debug);
+    $local_file = download_item($item);
+  }
 
-  printf("%s\n", $path);
+  die;
 }
+
+# search for files that shouldn't be in the data directory
 
